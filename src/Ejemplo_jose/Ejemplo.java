8730003344be3ba32812e8fa69f42a56ea99b6da
jose ordoñez <30303493+jose3201@users.noreply.github.com>
2020-06-27 18:44:17 -0600

    mod

---
+++ src/Ejemplo_jose/Ejemplo.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejemplo_jose;
+
+import Clientes.*;
+import Conductores.*;
+import Veiculo.*;
+
+/**
+ *
+ * @author josed
+ */
+public class Ejemplo {
+    
+/*
+       public static TablaH tablita=new TablaH();
+    public static ListaC lista=new ListaC();
+    public static ArbolB t=new ArbolB(3);
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /* para mandar a llamar a la tabla hash que es la variablke tablita 
+        NodoH encontrado=tablita.Busqueda("aqui mandas el dpi del cliente ");   }
+        para conductore es asi:
+        NodoC encontado= lista.Buscar("aqui pones el dpi del conductor a buscar");
+        de alli ya buscar el get que queras 
+        */
+      //dfdfdf
+      /*  MenuV nuevo= new MenuV();
+        nuevo.setVisible(true);
+       /* MenuC nuevo=new MenuC();
+        nuevo.setVisible(true);*/
+  /*  }*/
+}
+
